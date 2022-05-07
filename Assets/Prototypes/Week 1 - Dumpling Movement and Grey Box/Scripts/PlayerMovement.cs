a8f27fe0b1c69adbb683ca7c16d8060bd92012e2
Kieran <kieranc.inbox@gmail.com>
2022-05-08 04:13:34 +1000

    Basic WASD Movement working with the new Input System working for the dumpling

--- Assets/Prototypes/Week 1 - Dumpling Movement and Grey Box/Scripts/PlayerMovement.cs
+++ Assets/Prototypes/Week 1 - Dumpling Movement and Grey Box/Scripts/PlayerMovement.cs
@@ -1,23 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace MainProject.Prototypes.Week1
 {
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement : MonoBehaviour, Dumpling.IPlayerActions
     {
         [SerializeField] private float movementSpeed = 2f;
         private Rigidbody myRigidbody;
-        // Start is called before the first frame update
-        void Start()
+        private Vector3 horizontalInput;
+        private Vector3 verticalInput;
+        private Vector3 direction;
+        private void Awake()
         {
             myRigidbody = GetComponent<Rigidbody>();
+            if (myRigidbody is null)
+                Debug.LogError("Rigidbody is NULL!");
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
-            
+            Vector3 finalDirection = new Vector3(direction.x, 0, direction.y);
+            myRigidbody.velocity = finalDirection * movementSpeed;
+        }
+
+        /// <summary>
+        /// I want this to move forward, back left and right.
+        /// </summary>
+        /// <param name="context">The input system WASD.</param>
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            direction = context.ReadValue<Vector2>();
+            Debug.Log(direction);
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 }
