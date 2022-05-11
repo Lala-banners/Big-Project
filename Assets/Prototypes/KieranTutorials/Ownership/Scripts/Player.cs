a08dd9ff2304b491a73189fd9bc1c0d59571729b
Kieran <kieranc.inbox@gmail.com>
2022-05-11 20:46:00 +1000

    Kieran Networking Tutorials working on

---
+++ Assets/Prototypes/KieranTutorials/Ownership/Scripts/Player.cs
@@ -0,0 +1,11 @@
+// Creator: Kieran
+// Creation Time: 2022/05/11 20:24
+using UnityEngine;
+
+namespace MainProject.Prototypes.KieranTutorials.Ownership
+{
+	public class Player : MonoBehaviour
+	{
+		
+	}
+}
