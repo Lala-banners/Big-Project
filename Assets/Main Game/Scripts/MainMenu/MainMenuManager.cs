babcc683275d37294b600e298aa0ba261cd8f83c
Kieran <kieranc.inbox@gmail.com>
2022-06-10 15:27:16 +1000

    Deleted shit

---
+++ Assets/Main Game/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,24 @@
+// Creator: Kieran
+// Creation Time: 2022/06/10 15:23
+using UnityEngine;
+
+namespace Main_Game.Scripts.MainMenu
+{
+	public class MainMenuManager : MonoBehaviour
+	{
+		public void PlayGame_Button()
+		{
+			
+		}
+		
+		public void Options_Button()
+		{
+			
+		}
+		
+		public void ExitGame_Button()
+		{
+			
+		}
+	}
+}
