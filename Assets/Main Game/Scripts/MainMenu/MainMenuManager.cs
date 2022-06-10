2221a251b5625bb1dac5b596bc1cd02ed8aac858
Kieran <kieranc.inbox@gmail.com>
2022-06-10 15:44:26 +1000

    Main Menu Working

--- Assets/Main Game/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Main Game/Scripts/MainMenu/MainMenuManager.cs
@@ -1,24 +1,31 @@
 // Creator: Kieran
 // Creation Time: 2022/06/10 15:23
+using BreadAndButter;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 namespace Main_Game.Scripts.MainMenu
 {
 	public class MainMenuManager : MonoBehaviour
 	{
-		public void PlayGame_Button()
+		[SerializeField] private string gamePlayScene;
+		public void PlayGame_Button()	
 		{
-			
+			SceneManager.LoadScene(gamePlayScene);
 		}
 		
 		public void Options_Button()
 		{
-			
+			Debug.Log("PLEASE CREATE THIS OPTIONS MENU");
 		}
 		
 		public void ExitGame_Button()
 		{
-			
+		#if UNITY_STANDALONE
+			Application.Quit();
+		#endif
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#endif
 		}
 	}
 }
