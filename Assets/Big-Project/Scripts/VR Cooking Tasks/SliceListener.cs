6f2cb3c47a1cd2a8c144298ca97feea34e7679d4
Lara Bannerman <lala.l.banno@hotmail.com>
2023-02-02 17:18:45 +1100

    Slicing works!!
    Needs further testing to get best combination of movement type in butcher's knife xr grab component

---
+++ Assets/Big-Project/Scripts/VR Cooking Tasks/SliceListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceListener : MonoBehaviour
+{
+    public Slicer slicer;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        slicer.isTouched = true;
+    }
+}
---
+++ Assets/Big-Project/Scripts/VR Cooking Tasks/Slicer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using EzySlice;
+
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Slicer : MonoBehaviour
+{
+    public Material materialAfterSlice;
+    public LayerMask sliceMask;
+    public bool isTouched;
+
+    private void Update()
+    {
+        if (isTouched)
+        {
+            isTouched = false;
+
+            Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
+
+            foreach (Collider objectToBeSliced in objectsToBeSliced)
+            {
+                SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+
+                GameObject upperHullGO = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+                GameObject lowerHullGO = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
+
+                upperHullGO.transform.position = objectToBeSliced.transform.position;
+                lowerHullGO.transform.position = objectToBeSliced.transform.position;
+
+                MakeItPhysical(upperHullGO);
+                MakeItPhysical(lowerHullGO);
+
+                Destroy(objectToBeSliced.gameObject);
+            }
+        }
+    }
+
+    private void MakeItPhysical(GameObject obj)
+    {
+        obj.AddComponent<MeshCollider>().convex = true;
+        obj.AddComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.Continuous;
+        obj.layer = LayerMask.NameToLayer("Sliceable");
+        obj.AddComponent<XRGrabInteractable>();
+    }
+
+    private SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
+    {
+        return obj.Slice(transform.position, transform.up, crossSectionMaterial);
+    }
+}
